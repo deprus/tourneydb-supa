f1a0a0aa17438b06619e1e9b6579631013819073
Denis <deprus97@gmail.com>
2023-08-09 00:38:52 +0300

    fix: try new api

--- src/app/api/tournaments/route.ts
+++ src/app/api/tournaments/route.ts
@@ -1,9 +1,6 @@
-import { createClient } from '@supabase/supabase-js';
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
 
-const supabase = createClient(
-  'https://lpyuvuvsjryzjjtivkdm.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxweXV2dXZzanJ5empqdGl2a2RtIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTA5MDIzOTUsImV4cCI6MjAwNjQ3ODM5NX0.3-U5mzFqTJyYoZoElpF9K1xq2_BLUGFiNKqxqgI94FM'
-);
+const supabase = createServerComponentClient({ cookies });
 
 export async function GET() {
   try {
--- src/app/tournaments/page.tsx
+++ src/app/tournaments/page.tsx
@@ -5,22 +5,31 @@ import { columns } from './columns';
 import { DataTable } from './data-table';
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import SheetAddTourney from '@/components/SheetAddTourney';
+import supabase from '@/utils/supabase';
 
 export default function DemoPage() {
   const { data, isLoading: isGetting } = useQuery({
     queryKey: ['tournaments'],
     queryFn: async () => {
-      const res = await fetch('/api/tournaments', { cache: 'no-store' });
-      return res.json();
+      const { data, error } = await supabase.from('Tournaments').select('*');
+
+      if (error) {
+        console.error(error);
+        throw new Error('Tournaments could not be loaded');
+      }
+
+      return data;
     },
   });
 
   if (isGetting) return <h1>Loading...</h1>;
 
+  const dataArray = data || [];
+
   return (
     <div className="container mx-auto py-10">
       <SheetAddTourney />
-      <DataTable columns={columns} data={data.data} />
+      <DataTable columns={columns} data={dataArray} />
     </div>
   );
 }
